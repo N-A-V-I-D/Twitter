fe05f4c13e8b3158298e0e5a583188d48b47da35
N-A-V-I-D <NC1939@nyu.edu>
2021-10-15 01:56:03 -0400

    Twitter part II

--- HomeTableViewController.swift
+++ HomeTableViewController.swift
@@ -20,13 +20,19 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
+        
         myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight  = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 147
         
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweet()
+    }
     
     @objc func loadTweet(){
         
@@ -79,15 +85,7 @@ class HomeTableViewController: UITableViewController {
             loadMoreTweets()
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
@@ -115,8 +113,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
-        
-        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
